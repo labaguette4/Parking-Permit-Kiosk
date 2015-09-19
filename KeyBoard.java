c57e13f1bd727359d9a688665178d65d9120cfe3
shayan90 <shayan.raf@gmail.com>
2015-09-19 19:48:42 -0400

    Update KeyBoard.java

--- KeyBoard.java
+++ KeyBoard.java
@@ -17,7 +17,7 @@ public class KeyBoard extends JFrame {
     private JButton[][] button;
     private static final String[][] key = {
         {"A", "B", "C", "D", "E","F","G","H","I"},
-        {"G", "K", "L", "N", "M","O","P","Q","R"}, 
+        {"J", "K", "L", "N", "M","O","P","Q","R"}, 
         {"S", "T", "U","V","W","X","Y","Z"},
             {"0","1", "2", "3", "4","5","6","7","8","9"},
            {"@","@yorku.ca","@gmail.com","@yahoo.com"},
